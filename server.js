98933af9b4dd90db98ca7e3bd8d11a2b7852590e
DevTheCoder555 <devgupta51006@gmail.com>
2026-03-17 00:37:20 +0530

    fixed mails service

--- server.js
+++ server.js
@@ -22,13 +22,14 @@ app.use(express.json())
 
 // ── Nodemailer (Gmail + App Password) ───────────────────────────────────
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-})
-
+    pass: process.env.EMAIL_PASS
+  }
+});
 // Verify connection on startup
 transporter.verify((err) => {
   if (err) {
